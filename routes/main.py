ae67164be830d4c4b8e139b1370f8035664623d1
ENG. Mohamed Samir <bbad01133@gmail.com>
2025-11-19 03:52:08 +0200

    Fix string quotes in main.py

--- routes/main.py
+++ routes/main.py
@@ -1,9 +1,7 @@
-# routes/main.py
-
 from flask import Blueprint, render_template
 
-main_bp = Blueprint('main', __name__)
+main_bp = Blueprint("main", __name__)
 
-@main_bp.route('/index')
+@main_bp.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
