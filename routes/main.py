2e78d035b608eefbdcd2eec5e193ff387e4ca683
ENG. Mohamed Samir <bbad01133@gmail.com>
2025-11-19 03:45:49 +0200

    Change main route to '/index'

--- routes/main.py
+++ routes/main.py
@@ -1,9 +1,9 @@
+# routes/main.py
+
 from flask import Blueprint, render_template
-from models import Seat, Booking
-from flask_login import login_required, current_user
 
 main_bp = Blueprint('main', __name__)
 
-@main_bp.route('/')
+@main_bp.route('/index')
 def index():
     return render_template('index.html')
